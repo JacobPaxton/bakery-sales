5b99d376ae5634625cf0327ff23c1a61eff06b13
JacobPaxton <jacob.paxton65@gmail.com>
2021-12-19 17:23:57 -0600

    add tuesday dropper function

--- wrangle.py
+++ wrangle.py
@@ -9,3 +9,18 @@ def prep_explore():
     sales = sales[sales.datetime.index < 2420]
 
     return sales, prices
+
+def drop_closed_tuesdays(resampled_day_df):
+    """ 
+        Business is closed on all Tuesdays except Christmas Eve,
+        Drop all non-Christmas Eve Tuesdays,
+        Return dataframe.
+    """
+    tuesdays = resampled_day_df.index.day_name() == 'Tuesday'
+    resampled_day_df[tuesdays].index.strftime('%m-%d') != '12-24'
+
+    days_to_drop = ((resampled_day_df.index.day_name() == 'Tuesday') & 
+                    (resampled_day_df.index != '2019-12-24'))
+    daily_no_tuesdays = resampled_day_df[days_to_drop == False]
+
+    return daily_no_tuesdays
